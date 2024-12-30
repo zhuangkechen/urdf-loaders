6a155740d957cbf580aebac3adb37fdf21a18e93
kevinzhuang <kevinzhuang@tencent.com>
2024-12-30 11:28:33 +0800

    update

--- javascript/example/bundle/src.a2b27638.js
+++ javascript/example/bundle/src.a2b27638.js
@@ -49098,7 +49098,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61981" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63043" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
